ad038a5c48ea56bf03bb510f7609231c3518c416
guillem2001 <gdolcet@almata.cat>
2021-03-25 17:37:18 +0100

    afegim la classe payer i definim una funcio, afegim tambe els spirites i animacions dels players

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,22 @@
-
+var player1 = null;
+var player2 = null;
 function setup() {
-    createCanvas(1500, 800);
+    createCanvas(1200, 800);
     bg = loadImage('img/pista.jpg');
-    dino = createSprite(150, 150);
-    santa = createSprite(1400, 150);
+    /*dino = createSprite(150, 150);
+    santa = createSprite(1100, 150);
     santa.addAnimation('walk','img/santa/Idle (1).png','img/santa/Idle (2).png','img/santa/Idle (3).png','img/santa/Idle (4).png','img/santa/Idle (5).png','img/santa/Idle (6).png','img/santa/Idle (7).png','img/santa/Idle (8).png','img/santa/Idle (9).png','img/santa/Idle (10).png','img/santa/Idle (11).png','img/santa/Idle (12).png','img/santa/Idle (13).png','img/santa/Idle (14).png','img/santa/Idle (15).png');
     dino.addAnimation('walk','img/dino/Idle (1).png','img/dino/Idle (2).png','img/dino/Idle (3).png','img/dino/Idle (4).png','img/dino/Idle (5).png','img/dino/Idle (6).png','img/dino/Idle (7).png','img/dino/Idle (8).png','img/dino/Idle (9).png','img/dino/Idle (10).png');
+    */
+    player1 = new Player('player1');
+    player2 = new Player('player2');
+    player1.playerInici('player1');
 }
+
 
 function draw() {
     background(bg);
-    santa.mirrorX(-1);
+    //santa.mirrorX(-1);
     //specify the animation instance and its x,y position
     //animation() will update the animation frame as well
     drawSprites();
