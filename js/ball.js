06dc685d7ecdab864a8b0372ff0ff043e60206df
guillem2001 <gdolcet@almata.cat>
2021-04-07 16:06:37 +0200

    afegim imatges a la bola per fer el sprite

--- js/ball.js
+++ js/ball.js
@@ -2,7 +2,7 @@ class Ball {
     constructor(){
         this.speed = 1;
         this.jugadorSprite = createSprite(600, 400);
-        this.jugadorSprite.addAnimation('ball','img/ball/ball.png');
+        this.jugadorSprite.addAnimation('ball','img/ball/ball.png','img/ball/ball2.png','img/ball/ball3.png','img/ball/ball4.png','img/ball/ball5.png','img/ball/ball6.png','img/ball/ball7.png','img/ball/ball8.png','img/ball/ball9.png','img/ball/ball10.png');
         this.jugadorSprite.changeAnimation('ball');
     }
     ballInici(){
