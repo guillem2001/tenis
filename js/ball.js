7fa04dad182311f0a70691136cea4e36b4bfcfc5
guillem2001 <gdolcet@almata.cat>
2021-04-08 17:23:01 +0200

    Afegim que la bola reboti en els personatges

--- js/ball.js
+++ js/ball.js
@@ -1,12 +1,35 @@
 class Ball {
     constructor(){
         this.speed = 1;
-        this.jugadorSprite = createSprite(600, 400);
-        this.jugadorSprite.addAnimation('ball','img/ball/ball.png','img/ball/ball2.png','img/ball/ball3.png','img/ball/ball4.png','img/ball/ball5.png','img/ball/ball6.png','img/ball/ball7.png','img/ball/ball8.png','img/ball/ball9.png','img/ball/ball10.png');
-        this.jugadorSprite.changeAnimation('ball');
+        this.direction = 1;
+        this.ballSprite = createSprite(600, 400);
+        this.ballSprite.addAnimation('ball','img/ball/ball.png','img/ball/ball2.png','img/ball/ball3.png','img/ball/ball4.png','img/ball/ball5.png','img/ball/ball6.png','img/ball/ball7.png','img/ball/ball8.png','img/ball/ball9.png','img/ball/ball10.png');
+        this.ballSprite.changeAnimation('ball');
     }
     ballInici(){
-        this.jugadorSprite.position.x = 600;
-        this.jugadorSprite.position.y = 400;
+        this.ballSprite.position.x = 600;
+        this.ballSprite.position.y = 400;
+        this.ballSprite.setSpeed(10,0);
     }
+
+    rebota(player) {
+        this.ballSprite.bounce(player, this.changeDirection());
+    }
+
+    moure(){
+        if(this.direction === 1){
+            this.ballSprite.position.x += 1;
+        }else{
+            this.ballSprite.position.x -= 1;
+        }
+    }
+
+    changeDirection(){
+        if (this.direction === 1){
+            this.direction = 0;
+        }else{
+            this.direction = 1;
+        }
+    }
+
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,9 @@
 var player1 = null;
 var player2 = null;
+var wallTop = null;
+var wallBottom = null;
 var ball = null;
+var MAX_SPEED = 10;
 function setup() {
     createCanvas(1200, 800);
     bg = loadImage('img/pista.jpg');
@@ -10,12 +13,56 @@ function setup() {
     player1.playerInici('player1');
     player2.playerInici('player2');
     ball.ballInici();
+    player1.jugadorSprite.immovable = true;
+    player2.jugadorSprite.immovable = true;
+
+    wallTop = createSprite(width/2, -30/2, width, 30);
+    wallTop.immovable = true;
+
+    wallBottom = createSprite(width/2, height+30/2, width, 30);
+    wallBottom.immovable = true;
 }
 
 
 function draw() {
     background(bg);
     drawSprites();
+    /*ball.rebotar(player1);
+    ball.rebotar(player2);
+    ball.ballsprite.collide(jugador1)*/
+    movePlayers();
+    ball.rebota(player1.jugadorSprite);
+    ball.rebota(player2.jugadorSprite);
+    ball.moure();
+
+    /*ball.collide(wallTop);
+    ball.collide(wallBottom);
+
+    let swing;
+    if(ball.collide(player1)) {
+        swing = (ball.position.y-player1.position.y)/3;
+        ball.setSpeed(MAX_SPEED, ball.getDirection()+swing);
+    }
+
+    if(ball.collide(player2)) {
+        swing = (ball.position.y-player2.position.y)/3;
+        ball.setSpeed(MAX_SPEED, ball.getDirection()-swing);
+    }
+
+    if(ball.position.x<0) {
+        ball.position.x = width/2;
+        ball.position.y = height/2;
+        ball.setSpeed(MAX_SPEED, 0);
+    }
+
+    if(ball.position.x>width) {
+        ball.position.x = width/2;
+        ball.position.y = height/2;
+        ball.setSpeed(MAX_SPEED, 180);
+    }*/
+}
+
+function movePlayers(){
     if (keyIsDown(UP_ARROW)) {
         player2.playerUp();
     }else if (keyIsDown(DOWN_ARROW)) {
